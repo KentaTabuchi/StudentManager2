9beadcdb4be80093c9a59a685f18ddf0272ceb10
田淵健太 <>
2019-07-13 14:47:07 +0900

    comfirm findbutton exit.

--- bin/main/static/js/index.js
+++ bin/main/static/js/index.js
@@ -2,12 +2,15 @@
  * 
  */
   function test(){
-	  alert("index.jsの読み込みに成功");
+	  alert("index.jsの読み込みに成功?");
   }
-
+  function OnButtonClick(){
+	  alert("findButtonClick");
+  }
 "use strict";
   function OnSelectChange() {
     selindex = document.getElementById("SelectBox").selectedIndex;
     if(!selindex==0)	
     	document.location.href = "/?select_id="+selindex;
   }
+
