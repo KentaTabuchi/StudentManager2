6b4863cf59849830a263395cbcd02e96cf1a97bf
田淵健太 <>
2019-07-13 23:10:15 +0900

    Add unlock filter button.

--- bin/main/static/js/index.js
+++ bin/main/static/js/index.js
@@ -1,19 +1,17 @@
-/**
- * 
- */
+//|---------------------------------------------------------------------
+//|This script is called by "index.html".This cannot use the other pages.
+//|---------------------------------------------------------------------
   function test(){
 	  alert("index.jsの読み込みに成功?");
   }
-  function OnButtonClick(){
-	  alert("findButtonClick");
+  function OnButton1Click(){
 	  select = document.getElementById("FindBox").selectedIndex;
 	  text = document.getElementById("FindText").value;
 	  url = "/?find_text="+text+"&find_select="+select;
-	  alert(url);
 	  document.location.href = url;
-	//	 @RequestParam(name="find_select,required=false")Integer find_select,
-	//	 @RequestParam(name="find_text,requred=false")String find_text,
-	  
+  }
+  function OnButton2Click(){
+	  document.location.href = "/?select_id="+1; // 1 means find all by id.
   }
 "use strict";
   function OnSelectChange() {
