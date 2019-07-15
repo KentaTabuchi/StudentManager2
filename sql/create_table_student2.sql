ea90ca8fe079224126267a9a29fe23101b028f57
田淵健太 <>
2019-07-15 13:14:37 +0900

    temp

---
+++ sql/create_table_student2.sql
@@ -0,0 +1,16 @@
+create table student(
+id bigint(20) primary key auto_increment,
+no varchar(8),
+name varchar(20),
+roma varchar(20),
+sex varchar(1),
+phone varchar(20),
+email varchar(20),
+postno varchar(9),
+address1 varchar(6),
+address2 varchar(10),
+address3 varchar(40),
+address4 varchar(30),
+birthday varchar(13),
+graduation varchar(5)
+);
